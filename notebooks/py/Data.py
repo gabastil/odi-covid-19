c78f6e4c55182e82e4dcc40fc94d34ba695a10fc
gabastil <glenn.abastillas@gmail.com>
2020-04-09 16:12:47 -0400

    CBP

---
+++ notebooks/py/Data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 13:54:48 2020
+
+@author: abastillasgl
+"""
+
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from glob import glob
---
+++ notebooks/py/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 13:43:14 2020
+
+@author: abastillasgl
+"""
+
